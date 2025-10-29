33363af142af221fa46b6df0c790688c6955529a
석 <magnoliabk27@gmail.com>
2025-10-30 00:38:19 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+document.getElementById('open01').addEventListener('click', openPopup01);
+document.getElementById('open02').addEventListener('click', openPopup02);
+document.getElementById('open03').addEventListener('click', openPopup03);
+document.getElementById('open04').addEventListener('click', openPopup04);
+document.getElementById('open05').addEventListener('click', openPopup05);
+document.getElementById('open06').addEventListener('click', openPopup06);
+document.getElementById('open07').addEventListener('click', openPopup07);
+document.getElementById('open08').addEventListener('click', openPopup08);
+document.getElementById('open09').addEventListener('click', openPopup09);
+document.getElementById('open10').addEventListener('click', openPopup10);
+
+
+function openPopup01() {
+    // console.log("opened a new window popup.");
+    window.open(
+        'poems/poem01.html', 
+        null, 
+        'popup, resizable=0, width=640, height=720, left=0, top=0, noopener'
+    );
+}
+
+function openPopup02() {
+    window.open(
+        'poems/poem02.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=50, top=10, noopener'
+    );
+}
+
+function openPopup03() {
+    window.open(
+        'poems/poem03.html', 
+        null, 
+        'popup, resizable=0, width=840, height=840, left=100, top=0, noopener'
+    );
+}
+
+function openPopup04() {
+    window.open(
+        'poems/poem04.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=150, top=10, noopener'
+    );
+}
+
+function openPopup05() {
+    window.open(
+        'poems/poem05.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=200, top=0, noopener'
+    );
+}
+
+function openPopup06() {
+    window.open(
+        'poems/poem06.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=250, top=10, noopener'
+    );
+}
+
+function openPopup07() {
+    window.open(
+        'poems/poem07.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=300, top=0, noopener'
+    );
+}
+
+function openPopup08() {
+    window.open(
+        'poems/poem08.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=350, top=10, noopener'
+    );
+}
+
+function openPopup09() {
+    window.open(
+        'poems/poem09.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=400, top=0, noopener'
+    );
+}
+
+function openPopup10() {
+    window.open(
+        'poems/poem10.html', 
+        null, 
+        'popup, resizable=0, width=640, height=840, left=450, top=10, noopener'
+    );
+}
+
+
+
+
---
+++ poems/index02.js
@@ -0,0 +1,60 @@
+// 여기서부터 popup windows
+function toggleText01() {
+    var moreText = document.getElementById("moreText01");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
+
+function toggleText02() {
+    var moreText = document.getElementById("moreText02");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
+
+function toggleText03() {
+    var moreText = document.getElementById("moreText03");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
+
+function toggleText04() {
+    var moreText = document.getElementById("moreText04");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
+
+function toggleText05() {
+    var moreText = document.getElementById("moreText05");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
+
+function toggleText06() {
+    var moreText = document.getElementById("moreText06");
+  
+    if (moreText.style.display === "none") {
+      moreText.style.display = "inline"; // 내용을 보이게 함
+    } else {
+      moreText.style.display = "none"; // 내용을 숨김
+    }
+}
